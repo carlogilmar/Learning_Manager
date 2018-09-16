7ed11fc4aaf54b03e554b5fc58403e9447355f08
carlogilmar <carlogilmar12@gmail.com>
2018-09-16 02:00:38 -0500

    Adding a HTTP request for get a firebase real database AKA Mongo in server

--- lib/escript_demo.ex
+++ lib/escript_demo.ex
@@ -1,7 +1,15 @@
 defmodule EscriptDemo do
-  def main(_args) do
-    ["Hello, ", :red, :bright, "world!"]
-    |> IO.ANSI.format
-    |> IO.puts
+
+  def main(args) do
+    IO.puts "hola a todos!!"
+    IO.inspect args
+    show_users()
   end
+
+  def show_users() do
+    response = HTTPoison.get! "https://gameofchats-db1b4.firebaseio.com/users.json"
+    body = Poison.decode!(response.body)
+    IO.inspect body
+  end
+
 end
