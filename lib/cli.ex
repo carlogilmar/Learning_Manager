22bae1e6a29b08baaffaabdd653dfd83866351b5
carlogilmar <carlogilmar12@gmail.com>
2018-09-17 00:32:21 -0500

    Show a beautiful description

--- lib/cli.ex
+++ lib/cli.ex
@@ -1,8 +1,12 @@
 defmodule Toille.Cli do
 
   alias Toille.CliOperation
+  alias Toille.Parser
 
   def main(_args) do
+		Parser.print_with_color "=====================", :color87
+		Parser.print_with_color " Le Toille App 🌟 !", :color228
+		Parser.print_with_color "=====================", :color87
     CliOperation.cli()
   end
 
