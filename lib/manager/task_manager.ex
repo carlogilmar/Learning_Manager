ed68c4f29fbcb2243619b45af69b1905a5ba5f99
carlogilmar <carlogilmar12@gmail.com>
2018-09-17 23:55:49 -0500

    Get the task by id and update status

--- lib/manager/task_manager.ex
+++ lib/manager/task_manager.ex
@@ -29,4 +29,15 @@ defmodule Etoile.TaskManager do
     %{ id: id, title: title, status: @todo, day: day, month: month, year: year }
   end
 
+  def find_task( tasks, task_id, status ) do
+    Enum.filter( tasks, fn task -> task["id"] == task_id end ) |> prepare_for_update( status )
+  end
+
+  def prepare_for_update( [], _ ), do: Parser.print_with_color " \n Invalid Task ID", :color198
+  def prepare_for_update( [ task ], status ) do
+    firebase_uuid = task["firebase_uuid"]
+    task_updated = Map.put( task, "status", status ) |> Map.delete("firebase_uuid")
+    { firebase_uuid, task_updated }
+  end
+
 end
