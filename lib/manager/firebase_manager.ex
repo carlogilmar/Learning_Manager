ed08bd794b34ac3f90949cf9d15710573a80dd7d
carlogilmar <carlogilmar12@gmail.com>
2018-09-17 23:55:36 -0500

    Adding put request to firebase

--- lib/manager/firebase_manager.ex
+++ lib/manager/firebase_manager.ex
@@ -1,6 +1,7 @@
 defmodule Etoile.FirebaseManager do
 
   alias Etoile.TaskManager
+  alias Etoile.Parser
 
 	def add_task( title ) do
     task =
@@ -19,6 +20,17 @@ defmodule Etoile.FirebaseManager do
       { id, attributes } = task
       Map.put( attributes, "firebase_uuid", id)
     end
+  end
+
+  def update_task( task_id, status ) do
+    { uuid, task }= show_tasks() |> TaskManager.find_task( task_id, status )
+    request = update_request( uuid, task )
+  end
+
+  def update_request( uuid, task) do
+    payload = task |> Poison.encode!
+    "https://gameofchats-db1b4.firebaseio.com/tasks/#{uuid}.json" |> HTTPoison.put( payload )
+		Parser.print_with_color "\n TASK #{task["title"]}... was set to: #{task["status"]} 🎉", :color214
   end
 
 end
