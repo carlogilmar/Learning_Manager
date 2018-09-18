70d5452390186602b14653419961fecee343c01e
carlogilmar <carlogilmar12@gmail.com>
2018-09-17 21:54:15 -0500

    Parse payload from firebase

--- lib/firebase_manager.ex
+++ lib/firebase_manager.ex
@@ -1,8 +1,5 @@
 defmodule Etoile.FirebaseManager do
 
-  alias Etoile.Parser
-  alias Etoile.TaskManager
-
 	def add_task( task ) do
 		payload = Poison.encode!(task)
 		{:ok, _} = HTTPoison.post "https://gameofchats-db1b4.firebaseio.com/tasks.json", payload
@@ -10,7 +7,14 @@ defmodule Etoile.FirebaseManager do
 
   def show_tasks() do
     response = HTTPoison.get! "https://gameofchats-db1b4.firebaseio.com/tasks.json"
-    Poison.decode!( response.body ) |> TaskManager.filter_tasks
+    Poison.decode!( response.body ) |> parser_payload
+  end
+
+  def parser_payload( payload ) do
+    for task <- payload do
+      { id, attributes } = task
+      Map.put( attributes, "firebase_uuid", id)
+    end
   end
 
 end
