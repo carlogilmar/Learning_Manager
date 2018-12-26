449bd56d54e5574ff91b0b6b8d1b84d71108f4c7
carlogilmar <carlogilmar12@gmail.com>
2018-12-25 22:43:35 -0600

    Refactor cli module

--- lib/cli.ex
+++ lib/cli.ex
@@ -7,32 +7,38 @@ defmodule Etoile.Cli do
 
   def main(args), do: start_cli(args)
 
-  def start_cli([]), do: CliSession.cli()
-
+  def start_cli([]), do: CliSession.cli() # Register User
   def start_cli(["--user", username]) do
-		Parser.print_with_color " Learning Manager 🌟    ", :color228
-    Parser.print_with_color " - - - - - - - - - - - -", :color87
-    Parser.print_with_color " Login with #{username} ", :color228
+    send_start_msg( username )
     response = UserManager.find_user( username )
     case response do
       {:not_found, _msg} ->
-        Parser.print_with_color "==============================================", :color87
-        Parser.print_with_color " #{username} not found                       ", :color228
-        Parser.print_with_color " Please sign up or login with user registered.", :color228
-        Parser.print_with_color "==============================================", :color87
+        send_not_found_msg( username )
       {:user_found, user} ->
-        IO.puts " Hay usuario papa!"
+        CliOperation.cli( user )
     end
+  end
+  def start_cli(_args), do: send_error_msg()
 
-    #CliOperation.cli()
+  defp send_start_msg( username ) do
+		Parser.print_with_color " Learning Manager 🌟    ", :color228
+    Parser.print_with_color " - - - - - - - - - - - -", :color87
+    Parser.print_with_color " Login with #{username} ", :color228
   end
 
-  def start_cli(_args) do
+  defp send_error_msg() do
 		Parser.print_with_color "=======================================", :color87
     Parser.print_with_color " ERROR: Option not found.              ", :color228
     Parser.print_with_color " Please login with your username:      ", :color228
     Parser.print_with_color "   > learning_manager --user username  ", :color228
 		Parser.print_with_color "=======================================", :color87
+  end
+
+  defp send_not_found_msg( username ) do
+    Parser.print_with_color "==============================================", :color87
+    Parser.print_with_color " #{username} not found                       ", :color228
+    Parser.print_with_color " Please sign up or login with user registered.", :color228
+    Parser.print_with_color "==============================================", :color87
   end
 
 end
