1c81206360235215b410c246ea4c50b9c040cfd3
carlogilmar <carlogilmar12@gmail.com>
2018-12-25 22:13:43 -0600

    Validation user session start

--- lib/cli.ex
+++ lib/cli.ex
@@ -6,16 +6,23 @@ defmodule Etoile.Cli do
 
   def main(args), do: start_cli(args)
 
-  def start_cli([]) do
-    CliSession.cli()
-  end
+  def start_cli([]), do: CliSession.cli()
 
-  def start_cli(args) do
+  def start_cli(["--user", username]) do
     IO.puts " ::Learning Manager:: Enviando usuario"
+    IO.inspect username
     #Parser.print_with_color "=====================", :color87
 		#Parser.print_with_color " Le Etoile App 🌟 !", :color228
 		#Parser.print_with_color "=====================", :color87
     #CliOperation.cli()
-    IO.inspect args
   end
+
+  def start_cli(_args) do
+		Parser.print_with_color "=======================================", :color87
+    Parser.print_with_color " ERROR: Option not found.              ", :color228
+    Parser.print_with_color " Please login with your username:      ", :color228
+    Parser.print_with_color "   > learning_manager --user username  ", :color228
+		Parser.print_with_color "=======================================", :color87
+  end
+
 end
