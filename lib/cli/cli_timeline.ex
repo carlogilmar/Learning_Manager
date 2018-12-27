c54a1b0f2a67959dc004ea4b9d1f5b976726c3b0
carlogilmar <carlogilmar12@gmail.com>
2018-12-26 18:06:45 -0600

    Adding options for add labels and places

--- lib/cli/cli_timeline.ex
+++ lib/cli/cli_timeline.ex
@@ -1,31 +1,34 @@
 defmodule Etoile.Cli.CliTimeline do
 
   alias Etoile.Parser
-	alias Etoile.FirebaseManager
-  alias Etoile.TaskManager
-  alias Etoile.ProjectManager
   alias Etoile.CalendarUtil
   alias Etoile.TimelineManager
+  alias Etoile.TagManager
 
-  def cli( user ), do: receive_command( user )
+  def cli( user ), do: execute_command( user )
 
   def display_menu( user ) do
     CalendarUtil.print_current_day()
     TimelineManager.find_active_timeline( user["username"] )
 		Parser.print_with_color "-----------------------------------------", :color87
-		Parser.print_with_color " 1. Add current week as timeline         ", :color228
-		Parser.print_with_color " 2. List timelines stored                ", :color228
-		Parser.print_with_color " 3. Show timeline                        ", :color228
+    Parser.print_with_color " 1. Add current week as timeline [ok]        ", :color228
+		Parser.print_with_color " 2. List timelines stored [ok]               ", :color228
+		Parser.print_with_color " 3. Show current timeline                ", :color228
+		Parser.print_with_color " 4. Show a timeline                      ", :color228
 		Parser.print_with_color "-----------------------------------------", :color87
-    Parser.print_with_color " (q) Quit app", :color87
+		Parser.print_with_color " (5) Add label (6) Show labels           ", :color228
+		Parser.print_with_color " (7) Add places (8) Show places          ", :color228
+		Parser.print_with_color "-----------------------------------------", :color87
+    Parser.print_with_color " (h) Show menu (q) Quit app", :color87
 		cli( user )
   end
 
-  def receive_command( user ) do
-    IO.gets("\n 🌟 >>> ")
-      |> Parser.parse_command()
+  def execute_command( user ) do
+    receive_command
       |> execute( user )
   end
+
+  def receive_command(), do: IO.gets("\n 🌟 >>> ") |> Parser.parse_command()
 
 	def execute( cmd, user ) do
     case cmd do
@@ -36,6 +39,20 @@ defmodule Etoile.Cli.CliTimeline do
         TimelineManager.get_all_from_user( user["username"] )
         cli(user)
       "3" ->
+        cli(user)
+      "4" ->
+        cli(user)
+      "5" ->
+        TagManager.add_label( user["username"] )
+        cli(user)
+      "6" ->
+        TagManager.list_labels( user["username"] )
+        cli(user)
+      "7" ->
+        TagManager.add_place( user["username"] )
+        cli(user)
+      "8" ->
+        TagManager.list_places( user["username"] )
         cli(user)
       "h" ->
         display_menu(user)
---
+++ lib/manager/tag_manager.ex
@@ -0,0 +1,15 @@
+defmodule Etoile.TagManager do
+
+  def add_label( username ) do
+    IO.puts "Adding label"
+  end
+  def list_labels( username ) do
+    IO.puts "Show labels"
+  end
+  def add_place( username ) do
+    IO.puts "Adding place"
+  end
+  def list_places( username ) do
+    IO.puts "Show places"
+  end
+end
