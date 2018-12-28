61349e2ff2fc99ed41059b9637442859072be6b0
carlogilmar <carlogilmar12@gmail.com>
2018-12-28 00:53:31 -0600

    Adding timeline command

--- lib/app/bot.ex
+++ lib/app/bot.ex
@@ -1,21 +1,56 @@
 defmodule Etoile.Bot do
+
   @bot :simple_bot
   def bot(), do: @bot
-
   use ExGram.Bot, name: @bot
+  alias Etoile.TimelineManager
   require Logger
 
-  command("echo")
+  command("timeline")
+  command("budgets")
+  command("notes")
+  command("all")
+  command("todo")
+  command("done")
+  command("wip")
   middleware(ExGram.Middleware.IgnoreUsername)
 
-  def handle({:text, text, _}, cnt) do
-    IO.puts "Bot respondiendo..."
-    IO.inspect text
-    cnt |> answer(" Aprendiendo IoT Primeros Pasos Demo:: Hola, ya te atiendo con Elixir 🤖")
+  def handle({:command, :timeline, %{text: username}}, cnt) do
+    timeline = get_active_timeline( username )
+    cnt |> answer( timeline )
   end
 
-  def handle({:command, :echo, %{text: t}}, cnt) do
-    cnt |> answer(t)
+  def handle({:command, :budgets, %{text: _text}}, cnt) do
+    cnt |> answer(" \n Active Timeline \n Here\n Here::")
+  end
+
+  def handle({:command, :notes, %{text: _text}}, cnt) do
+    cnt |> answer(" \n Active Timeline \n Here\n Here::")
+  end
+
+  def handle({:command, :tasks, %{text: _text}}, cnt) do
+    cnt |> answer(" \n Active Timeline \n Here\n Here::")
+  end
+
+  def handle({:command, :todo, %{text: _text}}, cnt) do
+    cnt |> answer(" \n Active Timeline \n Here\n Here::")
+  end
+
+  def handle({:command, :wip, %{text: _text}}, cnt) do
+    cnt |> answer(" \n Active Timeline \n Here\n Here::")
+  end
+
+  def handle({:command, :done, %{text: _text}}, cnt) do
+    cnt |> answer(" \n Active Timeline \n Here\n Here::")
+  end
+
+  defp get_active_timeline( "" ), do: " Learning Manager :: Adding your current username "
+  defp get_active_timeline( username ) do
+    [{_id, active}] = TimelineManager.find_active_timeline( username )
+    case active do
+      [] -> " \n There isn't an active timeline. Please register timeline in learning manager."
+      _active -> " \n Active Timeline \n Username: #{username} \n Week #{active["week"]} - #{active["year"]}"
+    end
   end
 
 end
