1ddae77a3d9e525315a216bfc34f56f059395c9f
carlogilmar <carlogilmar12@gmail.com>
2019-01-02 22:09:06 -0600

    Show all history from a single timeline

--- lib/manager/history_manager.ex
+++ lib/manager/history_manager.ex
@@ -2,13 +2,16 @@ defmodule Etoile.HistoryManager do
 
   alias Etoile.BudgetManager
   alias Etoile.NoteManager
+  alias Etoile.TaskManager
   alias Etoile.Parser
 
   def display_timeline_history( week, year, username ) do
-    Parser.print_with_color " ::: H I S T O R Y :: Week #{week} Year #{year}  ::: ", :color228
+    Parser.print_with_color " ::: H I S T O R Y :: Week #{week} Year #{year}  ::: ", :color167
     BudgetManager.get_budgets( username, week, year ) |> BudgetManager.print_budgets()
-    Parser.print_with_color " - - - - - - - - - - - - - - - -", :color228
+    Parser.print_with_color " - - - - - - - - - - - - - - - -", :color167
     NoteManager.get_notes( username, week, year ) |> NoteManager.print_notes()
+    Parser.print_with_color " - - - - - - - - - - - - - - - -", :color167
+    TaskManager.display_tasks_history( username, week, year )
   end
 
 end
